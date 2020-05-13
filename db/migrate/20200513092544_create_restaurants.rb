ac6ddfe2d585deadbc9ff91a0310abf2f4e99a77
William GUILLERM <guillerm.william@gmail.com>
2020-05-13 11:27:11 +0200

    so

---
+++ db/migrate/20200513092544_create_restaurants.rb
@@ -0,0 +1,11 @@
+class CreateRestaurants < ActiveRecord::Migration[6.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :address
+      t.integer :phone_number
+      t.string :reviews
+
+      t.timestamps
+    end
+  end
+end
